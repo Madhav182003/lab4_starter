d1b042a87c73a9779ccd1a78a3fb37433142e0cf
mbansal03 <122562063+mbansal03@users.noreply.github.com>
2025-04-28 18:11:18 -0700

    bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,6 +7,10 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
+
+        if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+            return false;
+        }
         let result = 0;
 
         result = num1 + num2;
