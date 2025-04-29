b050cd12c9a3159de626d8a091f9ef9d2a03859a
mbansal03 <122562063+mbansal03@users.noreply.github.com>
2025-04-28 18:03:18 -0700

    fixed bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -27,7 +27,9 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    
+    if(length == 0){
+        return false;
+    }
     for(let i = 0; i < length; i++) {
         let discountedPrice = 0;
         discountedPrice += prices[i] * (1 - discount);
